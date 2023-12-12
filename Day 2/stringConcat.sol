674df2df66680685815c33769377962af4f68003
Hritam Shrivatava <59762864+hriztam@users.noreply.github.com>
2023-12-12 21:52:21 +0530

    Add files via upload

---
+++ Day 2/stringConcat.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ChristmasContract {
+    function combining(string memory str) public pure returns (string memory) {
+        return string.concat("Merry Christmas ", str);
+    }
+}
