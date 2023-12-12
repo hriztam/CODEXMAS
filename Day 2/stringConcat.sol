0a0b7392b1ebcc7f50741102277c8fa8a8ee2b1e
Hritam Shrivatava <59762864+hriztam@users.noreply.github.com>
2023-12-12 22:04:37 +0530

    Update stringConcat.sol

--- Day 2/stringConcat.sol
+++ Day 2/stringConcat.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.0;
 
 contract ChristmasContract {
     function combining(string memory str) public pure returns (string memory) {
-        return string.concat("Merry Christmas ", str);
+        return string.concat(str, " Merry Christmas!");
     }
 }
