b62e4e9c58f9192ce19c85a2131e6da522c0c206
Hritam Shrivatava <59762864+hriztam@users.noreply.github.com>
2023-12-12 13:29:29 +0530

    Add files via upload

---
+++ Day 1/greeting.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MerryChristmas {
+    function sayMerryChristmas() public pure returns (string memory) {
+        return "Ho, Ho, Ho, Merry Christmas!";
+    }
+}
+
+// deployed contract tx https://sepolia.etherscan.io/tx/0x6d7c24d4dc5ed9ac096eb7acdd723c02e614d45ffb5d8e7252f96ffeeef2af23
+
+// deployed contract address 0x702d90246a0f4d23d5156cd96765bc081edf55ec
