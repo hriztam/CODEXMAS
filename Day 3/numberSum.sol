19d52a9c627edadc29d7f36caa119eb753fa6f52
Hritam Shrivatava <59762864+hriztam@users.noreply.github.com>
2023-12-13 21:50:04 +0530

    Add files via upload

---
+++ Day 3/numberSum.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract NumberSum {
+    function Sum(uint256 num1, uint256 num2) public pure returns (uint256) {
+        uint256 result = num1 + num2;
+        return result;
+    }
+}
